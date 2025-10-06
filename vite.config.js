062b793b36d4ad0add4b453932468193b4082f16
Joao <joao.jr500@gmail.com>
2025-10-05 23:56:05 -0300

    reconfigurando para deploy na vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { copyFileSync, existsSync } from 'fs'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
